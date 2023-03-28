08d18056e0f93f24d8a3e13bc95085857c71db38
Mike Panitz <michpa@hotmail.com>
2023-03-27 21:30:31 -0700

    Initial Commit

---
+++ DemoApp1/DemoApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp1App.swift
+//  DemoApp1
+//
+//  Created by Mike Panitz on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
