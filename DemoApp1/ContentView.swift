10f61d403f3117f224450bf87f3be9744194661f
Tori Lew <torirlew@gmail.com>
2023-03-28 17:26:19 -0700

    Experiment with XCode

--- DemoApp1/ContentView.swift
+++ DemoApp1/ContentView.swift
@@ -13,7 +13,11 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            
             Text("Hello, world!")
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.pink)
         }
         .padding()
     }
