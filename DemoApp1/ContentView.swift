b009ab2dac0e20e9c686f9559e905a31cc4e0922
Tori Lew <torirlew@gmail.com>
2023-03-28 18:39:32 -0700

    Reflection

--- DemoApp1/ContentView.swift
+++ DemoApp1/ContentView.swift
@@ -5,6 +5,16 @@
 //  Created by Mike Panitz on 3/27/23.
 //
 
+//REFLECTION
+/* Try generating code and seeing how it works
+        1C. Regenerating the response didn't change the code.
+ Reflect on the process of using AI to generate code for you
+    1.
+        A. It was kind of difficult understanding how the AI responded to the prompt. And how it understood what I wanted it to do since my assumtion was different than the AI's.
+        B. The more specific you want the code, the more detailed the prompt has to be. If you have a general idea of what the code should look like then it can at least give a starting point.
+        C. I am worried that what I say will lead it to go off in a tangent and not give me what I am looking for, but if I pay attention and read all of the code before applying it to mine then I think it will be a very useful tool to gain more skills.
+ */
+
 import SwiftUI
 
 struct ContentView: View {
