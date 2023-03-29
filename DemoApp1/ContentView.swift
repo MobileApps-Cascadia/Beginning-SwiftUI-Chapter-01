ed7a53066078fd05a688e6dea959b71f8a08fa7b
Kenny <kennypluchau@gmail.com>
2023-03-29 11:49:59 -0700

    made edit to comment lines

--- DemoApp1/ContentView.swift
+++ DemoApp1/ContentView.swift
@@ -8,6 +8,8 @@
 // as discussed in class this seems to do easy to moderate code fairly easily. This fact really intruiges me. Because of this just knowing what to look for will save so much time as I will now need to sift through A LOT LESS stack overflow.
 // I am excited to see how this advances to ease a programmers stress and lessen the time to create their projects. Also if this advances enough maybe AAA game companies will finally get their games out on time.
 
+// overal I would say that it is moderatly easy to us chatGPT for code. I would likely use it after awhile of more experimenting.
+
 import SwiftUI
 
 struct ContentView: View {
